f40e6d1e627af9779cf7e07cc2969339f0a7cac8
Koen De Groot <koendegroot84@gmail.com>
2022-08-09 17:06:04 +0200

    added  custom toast messages

--- EntertainmentWebApp/Client/Store/State/MediaState.cs
+++ EntertainmentWebApp/Client/Store/State/MediaState.cs
@@ -4,8 +4,14 @@ namespace EntertainmentWebApp.Client.Store.State;
 
 public class MediaState : RootState
 {
-    public MediaState(bool isLoading, string? currentErrorMessage, IEnumerable<Media>? medias, Media? media) : base(
-        isLoading, currentErrorMessage)
+    public MediaState(
+        bool isLoading,
+        string? currentErrorMessage,
+        IEnumerable<Media>? medias,
+        Media? media
+    )
+        : base(
+            isLoading, currentErrorMessage)
     {
         Medias = medias;
         Media = media;
